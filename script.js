5e04de39c57b176f34883d5afcdfb3c8e154b988
Tim Mueller <trmueller5@gmail.com>
2023-06-03 12:25:46 -0500

    add test blackOutGame

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
 const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
+h2.textContent = `This content added by JavaScript`;
 
 document.getElementById("center").appendChild(h2);
+
+
+
+// Black Out Game Code
+const blackOutGameElementRed = document.getElementById("red");
+
+    blackOutGameElementRed.addEventListener(`mouseenter`, function() {
+    blackOutGameElementRed.style.background = "#FFFFFF";
+    });
+    blackOutGameElementRed.addEventListener(`mouseleave`, function() {
+    blackOutGameElementRed.style.background = "#000000";
+    });
